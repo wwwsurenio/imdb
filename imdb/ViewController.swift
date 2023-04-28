9f13ab650430ad332e7a16b02df9fca0e2c22730
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-04-28 12:03:38 +0200

    Initial Commit

---
+++ imdb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imdb
+//
+//  Created by User01 on 28/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
