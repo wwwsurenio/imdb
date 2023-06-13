cbe503cfee08bbae7e5625df1080c949202d8c9e
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-06-13 15:45:26 +0200

    attempt to modify the func recievedData gives me error "Cannot assign value of type 'MovieModel' to type 'MovieDetailsModel'"

--- imdb/UI/View/MovieDetailsViewController.swift
+++ imdb/UI/View/MovieDetailsViewController.swift
@@ -9,11 +9,11 @@ import Foundation
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
-    private let movie: MovieModel
+    private var movie: MovieDetailsModel
     private let titleLabel: UILabel = UILabel()
     private let overviewLabel: UILabel = UILabel()
-    
     private let movieDetailsController: MovieDetailsControllerInput
+    let tableView: UITableView = UITableView(frame: .zero, style: .plain)
     
     init(movie: MovieModel, movieDetailsController: MovieDetailsControllerInput) {
         self.movie = movie
@@ -36,16 +36,25 @@ class MovieDetailsViewController: UIViewController {
 }
 
 extension MovieDetailsViewController: MovieDetailsControllerDelegate {
-    func receivedData(movieDetailsModel: MovieDetailsModel) {
+    
+//    func receivedData(movieDetailsModel: MovieDetailsModel) {
+//
+//
+//    }
+//
+//    func receivedData(movieDetailsModels: [MovieDetailsModel]) {
+//        guard let movieDetailsModel = movieDetailsModels.first else {
+//            return
+//        }
+//
+//        DispatchQueue.main.async { [weak self] in
+//            self?.overviewLabel.text = "Movie Overview: \(movieDetailsModel.movieOverview)"
+//        }
+//    }
+    
+    func recievedData(movieDetailsModel: MovieDetailsModel) {
+        movie = movieDetailsModel
+        tableView.reloadData()
     }
     
-    func receivedData(movieDetailsModels: [MovieDetailsModel]) {
-        guard let movieDetailsModel = movieDetailsModels.first else {
-            return
-        }
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.overviewLabel.text = "Movie Overview: \(movieDetailsModel.movieOverview)"
-        }
-    }
 }
