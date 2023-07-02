963d523d32599580796fe3deb67a24e36f53a5ee
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-07-02 13:47:08 +0200

    Trying to switch the favoritesVC with the main navigation, but get an error with the initialization of MovieViewController in the Tabs class

--- imdb/UI/View/MainTabBarController.swift
+++ imdb/UI/View/MainTabBarController.swift
@@ -11,17 +11,17 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let favoriteVC = FavoritesViewController()
+        let navigationVC = MovieViewController()
         let profileVC = ProfileViewController()
         
         
         let profileImage = UIImage(systemName: "person.crop.circle")
-        let favoriteImage = UIImage(systemName: "safari")
+        let navigationImage = UIImage(systemName: "safari")
         
         profileVC.tabBarItem = UITabBarItem(title: "Profile", image: profileImage, tag: 0)
-        favoriteVC.tabBarItem = UITabBarItem(title: "Favorites", image: favoriteImage, tag: 1)
+        navigationVC.tabBarItem = UITabBarItem(title: "Favorites", image: navigationImage, tag: 1)
         
-        setViewControllers([favoriteVC, profileVC], animated: false)
+        setViewControllers([navigationVC, profileVC], animated: false)
     }
 }
 
